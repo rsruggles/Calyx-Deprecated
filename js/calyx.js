c491155ec4045b336dbbf8424ec80521c99c1f35
rsruggles <rs.ruggles@gmail.com>
2016-07-04 00:00:03 -0500

    Hover Rollback
    Revert back to hover only, since it apparently works as onClick on
    mobile devices.

--- js/calyx.js
+++ js/calyx.js
@@ -2,6 +2,3 @@
 function navToggle() {
     document.getElementsByClassName("nav-menu")[0].classList.toggle("mobile-nav");
 }
-function dropdownToggle() {
-    document.getElementsByClassName("dropdown")[0].classList.toggle("dropdown-show");
-}
