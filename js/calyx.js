2d9ee0d1ef419a996dd2e1cbdd96834373d9408e
John Uberbacher <johnuberbacher@gmail.com>
2016-07-04 18:39:59 -0400

    Updates to mobile nav
    Mobile nav example now functions properly

--- js/calyx.js
+++ js/calyx.js
@@ -2,3 +2,7 @@
 function navToggle() {
     document.getElementsByClassName("nav-menu")[0].classList.toggle("mobile-nav");
 }
+
+function navExampleToggle() {
+    document.getElementsByClassName("nav-menu-example")[0].classList.toggle("mobile-nav");
+}
